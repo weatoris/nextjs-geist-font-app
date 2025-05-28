6b1a1bf27f2494e11a0a5773a64e72ae29298df4
weatoris <david@horehled.cz>
2025-05-28 15:06:49 +0200

    Add .next/static/chunks/src_app_page_tsx_9148afbf._.js 1748437609020

---
+++ .next/static/chunks/src_app_page_tsx_9148afbf._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_b6e550ee._.js"
+],
+    source: "dynamic"
+});
